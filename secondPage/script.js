a7474b96d14913c8a43c89fce1f9b73bc090396b
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2020-10-16 14:33:27 +0200

    added some things

---
+++ secondPage/script.js
@@ -0,0 +1,12 @@
+
+
+// window.onload = function(){
+
+//     let langParagraph = document.getElementById('lang');
+//     let selectedOption = document.getElementById('dropdownMenuButton');
+    
+//         var result = selectedOption.options[selectedOption.selectedIndex].text;
+//         console.log(result);
+//         langParagraph.innerText = result;
+    
+// }
